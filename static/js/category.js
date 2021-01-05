202c6f467204f08357609919ff8eece29a847253
bingbingpa <guriguri1576@gmail.com>
2021-01-06 00:02:43 +0900

    fix: category newline bug 수정

--- static/js/category.js
+++ static/js/category.js
@@ -1,14 +1,9 @@
-/**
- * 页面ready方法
- */
 $(document).ready(function() {
     categoryDisplay();
 });
 
 /**
- * 分类展示
- * 点击右侧的分类展示时
- * 左侧的相关裂变展开或者收起
+ * 카테고리 표시
  * @return {[type]} [description]
  */
 function categoryDisplay() {
@@ -24,15 +19,14 @@ function selectCategory(){
     var thisId = decodeURIComponent(window.location.hash.substring(1));
     var allow = true;
     for(var i in exclude){
-        if(thisId == exclude[i]){
+        if(thisId === exclude[i]){
             allow = false;
             break;
         }
     }
     if(allow){
-        var cate = thisId;
-        $("section[post-cate!='" + cate + "']").hide(200);
-        $("section[post-cate='" + cate + "']").show(200);
+        $("section[post-cate!='" + thisId + "']").hide(200);
+        $("section[post-cate='" + thisId + "']").show(200);
     } else {
         $("section[post-cate='All']").show();
     }
