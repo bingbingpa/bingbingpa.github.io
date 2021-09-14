e7584bce5a012299debbc885e85328718327bd5e
bingbingpa <guriguri1576@gmail.com>
2021-09-14 22:01:48 +0900

    fix: 메인 매트릭스 수정

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import { FaArrowDown } from "react-icons/fa/";
 import config from "../../../content/meta/config";
@@ -7,26 +7,28 @@ import "./hero.scss";
 
 const Hero = props => {
   const { scrollToContent } = props;
+  const canvasRef = useRef();
+
   useEffect(() => {
-    const c = document.getElementById("matrixCanvas");
-    const ctx = c.getContext("2d");
-    c.height = window.innerHeight;
-    c.width = window.innerWidth - 15;
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth - 15;
     const texts = config.matrixTest.split("");
     const fontSize = 12;
-    const columns = c.width / fontSize;
+    const columns = canvas.width / fontSize;
     const drops = [];
     for (let x = 0; x < columns; x++) drops[x] = 1;
 
     function draw() {
       ctx.fillStyle = "rgba(0, 0, 0, 0.05)";
-      ctx.fillRect(0, 0, c.width, c.height);
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
       ctx.fillStyle = "#490";
       ctx.font = fontSize + "px arial";
       for (let i = 0; i < drops.length; i++) {
         let text = texts[Math.floor(Math.random() * texts.length)];
         ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-        if (drops[i] * fontSize > c.height || Math.random() > 0.98) drops[i] = 0;
+        if (drops[i] * fontSize > canvas.height || Math.random() > 0.98) drops[i] = 0;
         drops[i]++;
       }
     }
@@ -37,7 +39,7 @@ const Hero = props => {
   return (
     <React.Fragment>
       <section className="hero">
-        <canvas id="matrixCanvas" className="matrix" />
+        <canvas ref={canvasRef} className="matrix" />
         <button className="button-matrix" onClick={scrollToContent} aria-label="scroll">
           <FaArrowDown />
         </button>
